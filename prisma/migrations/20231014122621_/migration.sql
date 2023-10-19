a39acecc1e3512fa58a56bfebee4b4de79a9cf4a
mtsteo <mateusteotonho@gmail.com>
2023-10-18 22:40:59 -0300

    delete migrations

--- prisma/migrations/20231014122621_/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `Produto` DROP FOREIGN KEY `Produto_empresaId_fkey`;
-
--- AlterTable
-ALTER TABLE `Produto` MODIFY `empresaId` VARCHAR(191) NULL;
-
--- AddForeignKey
-ALTER TABLE `Produto` ADD CONSTRAINT `Produto_empresaId_fkey` FOREIGN KEY (`empresaId`) REFERENCES `Empresa`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
