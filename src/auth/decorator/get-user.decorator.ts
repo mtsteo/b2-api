dab3aedf66cab1568efe7df9f235aa38e14a7b92
mtsteo <mateusteotonho@gmail.com>
2023-10-18 22:40:12 -0300

    getID

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -1,7 +1,7 @@
 
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const GetUser = createParamDecorator(
+export const GetUserId = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     return request.user.id;
