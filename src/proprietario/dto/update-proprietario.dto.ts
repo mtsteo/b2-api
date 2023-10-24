3c1a26bc5ddf2b0583d075fcb6d047c91f6013f0
mtsteo <mateusteotonho@gmail.com>
2023-10-24 15:28:21 +0000

    fix

--- src/proprietario/dto/update-proprietario.dto.ts
+++ src/proprietario/dto/update-proprietario.dto.ts
@@ -1,9 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProprietarioDto } from './create-proprietario.dto';
-import { IsString } from 'class-validator';
 
 export class UpdateProprietarioDto extends PartialType(CreateProprietarioDto) {
-    @IsString()
-    colaboradorId :string
 }
 
